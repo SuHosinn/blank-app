f06b3430f57612b25271d4d957598495cdf673ec
SuHosinn <113075296+SuHosinn@users.noreply.github.com>
2024-09-07 15:23:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,17 @@ st.write("텍스트 프롬프트를 입력하고 AI 이미지를 생성하세요
 # 텍스트 입력
 prompt = st.text_input("프롬프트를 입력하세요:")
 
+st.write("생성하는 이미지의 사이즈 크기도 입력해주세요.")
+size = st.text_input("사이즈 크기를 입력하세요 (예, 512, 1024, ...) : ")
+
+
 if st.button("이미지 생성"):
     if prompt:
         try:
             kwargs = {
                 "prompt": prompt,
                 "n":1,
-                "size":"1024x1024"
+                "size": '"' + size + 'x' + size + '"'     #"1024x1024"
             }
 
             # OpenAI API를 사용하여 이미지 생성
