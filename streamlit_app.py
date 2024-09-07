de95d83460d9476323276cfc6cfcdb185ed0a65a
SuHosinn <113075296+SuHosinn@users.noreply.github.com>
2024-09-07 15:48:37 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ prompt = st.text_input("프롬프트를 입력하세요:")
 
 st.write("생성하는 이미지의 사이즈 크기도 입력해주세요.")
 size = st.text_input("사이즈 크기를 입력하세요 (예, 512, 1024, ...) : ")
-size = f"{size_input}x{size_input}" if size.isdigit() else None
+size = f"{size}x{size}" if size.isdigit() else None
 st.write(size)
 
 if st.button("이미지 생성"):
